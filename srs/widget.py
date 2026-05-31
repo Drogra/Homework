81583abc6dff8f88d03c3f1a29885a41cc0185dc
Drogra <onekeep2@yandex.ru>
2026-05-31 19:56:43 +0500

    fix bug

--- srs/widget.py
+++ srs/widget.py
@@ -1,5 +1,6 @@
 from typing import Any
-import masks
+
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(data: Any) -> str:
@@ -8,31 +9,24 @@ def mask_account_card(data: Any) -> str:
     Выводит тип карты/счет и замаскированный номер.
 
     """
-    #Разделяем строку на тип и номер
     type_and_number = data.split()
-    type_ = ' '.join(type_and_number[0:-1])
+    type_ = " ".join(type_and_number[0:-1])
     number_ = type_and_number[-1]
 
-    #Маскируем с помощью ранее созданных функций
-    if type_ == 'Счет':
-        #Маскировка для счета
-        return f'{type_} {masks.get_mask_account(number_)}'
+    if type_ == "Счет":
+        return f"{type_} {get_mask_account(number_)}"
     else:
-        #Маскировка для карт
-        return f'{type_} {masks.get_mask_card_number(number_)}'
+        return f"{type_} {get_mask_card_number(number_)}"
 
 
-def get_date(date_str):
+def get_date(date_str: Any) -> str:
     """
     Преобразует строку с датой из формата 'ГГГГ-ММ-ДДТЧЧ:ММ:СС.СССССС'
     в формат 'ДД.ММ.ГГГГ'.
 
     """
-    # Разделяем строку по символу 'T', чтобы получить только дату
-    date_part = date_str.Split('T')[0]
+    date_part = date_str.Split("T")[0]
 
-    # Разделяем дату по дефисам
-    year, month, day = date_part.Split('-')
+    year, month, day = date_part.Split("-")
 
-    # Формируем строку в нужном формате
     return f"{day}.{month}.{year}"
